1f388c15dd9b7939adc5a0834be92686f25687f7
Bartosz Wołodźko <bartekwolodzko@gmail.com>
2022-10-05 18:02:45 +0200

    done

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,41 +6,33 @@
 #include <vector>
 #include <cmath>
 
-using mojamapa_t = std::map<std::string, std::vector<double>>;
 using mojafunkcja_t = std::function<std::double_t (std::vector<double>)>;
 
-void wypisz(mojamapa_t mapa, mojafunkcja_t fun) {
+void wypisz(std::vector<double> wartosci, std::string klucz) {
   using namespace std;
-  for (auto kv : mapa) {
-    auto [k, v] = kv;
-    cout << "klucz: " << k << "; wartosc " << fun(v) << endl;
-  }
+    map<string, mojafunkcja_t> formatery;
+    formatery["sin"] = [](vector<double> x) { return sin(x[0]); };
+    formatery["add"] = [](vector<double> x) { return x[0] + x[1]; };
+    formatery["mod"] = [](vector<double> x) { return (int)x[0] % (int)x[1]; };
+  cout << "Wynik "<< klucz <<": " << formatery[klucz](wartosci) << endl;
 }
 
 int main(int argc, char **argv) {
   using namespace std;
-  map<string, mojafunkcja_t> formatery;
-  formatery["sin"] = [](vector<double> x) { return sin(x[0]); };
-  formatery["add"] = [](vector<double> x) { return x[0] + x[1]; };
-  formatery["mod"] = [](vector<double> x) { return (int)x[0] % (int)x[1]; };
-
   try {
     vector<string> argumenty(argv, argv + argc);
-
     string klucz = argumenty[1];
-
     vector<double> wartosci;
     for (int i = 2; i < argc; i++) {
       wartosci.push_back(stod(argumenty[i]));
     }
-    mojamapa_t mojamapa = {{klucz, wartosci}};
-    wypisz(mojamapa, formatery[klucz]);
+    wypisz(wartosci, klucz);
     } catch (exception &e) {
       if (argc == 1) {
         cout << "sin x - oblicza sinus od x\n add x y - oblicza sume\n mod x y - oblicza x%y" << endl;
       } else {
         cout << "Niepoprawne argumenty" << endl;
-          cout << "Blad: " << e.what() << endl;
+        cout << "Blad: " << e.what() << endl;
       }
       return 1;
     }
